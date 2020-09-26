bf971d9730f22b84747105b6d795c5c7bf7cda55
miguelangeltorresfp <miguelangeltorresfp@users.noreply.github.com>
2020-09-26 02:00:07 +0000

    deploy: 4bc06ba218f97aac2f4110df22341b47fc1716ae

---
+++ precache-manifest.de1bbb3f1a86dfdb553bea3aa5732c13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8178a58482a7faeda8bf906d8a253c35",
+    "url": "/semantic-release-example/index.html"
+  },
+  {
+    "revision": "0a3f8f0bf13e934f22d3",
+    "url": "/semantic-release-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c5930a8cda74a9cea4e",
+    "url": "/semantic-release-example/static/js/2.ba88fe5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/semantic-release-example/static/js/2.ba88fe5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3f8f0bf13e934f22d3",
+    "url": "/semantic-release-example/static/js/main.d826cfaa.chunk.js"
+  },
+  {
+    "revision": "0ac30650fac37110e60b",
+    "url": "/semantic-release-example/static/js/runtime-main.321f3187.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/semantic-release-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
